6cf8b412a26bdd044f7ba5c040b431a233ddcd82
Sujes71 <jesus17.pg@gmail.com>
2023-08-21 03:55:41 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AshbornClick
+{
+    partial class AshbornClick
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(175, 63);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(351, 63);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Speed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(377, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Multiply";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(511, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 20);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Press";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // AshbornClick
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 131);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox3);
+            this.Name = "AshbornClick";
+            this.Text = "AshbornClick";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace AshbornClick
+{
+    public partial class AshbornClick : Form
+    {
+    public struct POINT_API
+    {
+        public int X;
+
+        public int Y;
+    }
+
+    private struct RECT
+    {
+        public int Left;
+
+        public int Top;
+
+        public int Right;
+
+        public int Bottom;
+    }
+
+    public static int xPos;
+
+    public static int yPos;
+
+    public static int x;
+
+    public static int y;
+
+    public static int count;
+
+    public static int timer;
+
+    public static int dedo = 0;
+
+    public static int random;
+
+    public static int status;
+
+    private Random Levi = new Random();
+
+    public static Point newPoint;
+
+    public static Point cursor;
+
+    private IntPtr value;
+
+    private IntPtr hwnd;
+
+    private POINT_API mouse;
+
+    private RECT infoWindow;
+
+    private static Random num = new Random();
+
+    [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto)]
+    public static extern void mouse_event(int dwflags, int dx, int dy, int cbuttons, int dwExtraInfo);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern bool GetCursorPos(out Point lpPoint);
+
+    [DllImport("user32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    private static extern bool GetAsyncKeyState(Keys vkeys);
+
+    [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern int SendMessage(IntPtr intptr_0, int int_1, int int_2, IntPtr int_3);
+
+    [DllImport("user32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    private static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out POINT_API lpPoint);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr WindowFromPoint(POINT_API p);
+
+    [DllImport("user32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    private static extern int GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int cch);
+ 
+        public AshbornClick()
+        {
+            InitializeComponent();
+        }
+
+    private void GetWindow()
+    {
+        checked
+        {
+            if (Convert.ToDouble(Convert.ToString((int)GetForegroundWindow())) != 0.0)
+            {
+                GetCursorPos(out mouse);
+                hwnd = WindowFromPoint(mouse);
+                GetWindowRect(hwnd, out infoWindow);
+                xPos = mouse.X - infoWindow.Left;
+                yPos = mouse.Y - infoWindow.Top;
+            }
+            if (xPos < 0)
+            {
+                xPos = 0;
+            }
+            if (yPos < 0)
+            {
+                yPos = 0;
+            }
+            Console.Write(xPos);
+            value = new IntPtr((yPos << 16) | (xPos & 0xFFFF));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            newPoint = default(Point);
+            GetCursorPos(out newPoint);
+            x = newPoint.X;
+            y = newPoint.Y;
+            Cursor.Position = new Point(xPos, yPos);
+            for (int i = 0; i < Convert.ToInt16(numericUpDown2.Value); i++)
+            {
+                mouse_event(2, xPos, yPos, 0, 0);
+                mouse_event(4, xPos, yPos, 0, 0);
+                count++;
+                textBox3.Text = Convert.ToString(Convert.ToInt16(textBox3.Text) + 1);
+            }
+            Cursor.Position = new Point(x, y);
+            Thread.Sleep(Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (GetAsyncKeyState(Keys.F1))
+                {
+                    timer1.Start();
+                }
+                else
+                {
+                    timer1.Stop();
+                }
+            }
+            else
+            {
+                if (GetAsyncKeyState(Keys.F1))
+                {
+                    timer1.Start();
+                }
+                if (GetAsyncKeyState(Keys.F2))
+                {
+                    timer1.Stop();
+                }
+                if (GetAsyncKeyState(Keys.F3))
+                {
+                    cursor = default(Point);
+                    GetCursorPos(out cursor);
+                    xPos = cursor.X;
+                    yPos = cursor.Y;
+                    GetWindow();
+                    //TEMPORAL. En un futuro habra imagenes.
+                    this.BackColor = Color.LimeGreen;
+                    this.FormBorderStyle = FormBorderStyle.FixedDialog;
+                }
+            }
+        }
+    }
+}
